80a473737eef77d55d5bf3dfedd1e89ba037d6af
Kingsley Owusu-Sekyere <k.owusu-sekyere.86@gmail.com>
2021-05-06 22:47:20 +0200

    simple client

--- notification-ui/src/app/app.module.ts
+++ notification-ui/src/app/app.module.ts
@@ -1,4 +1,7 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -8,6 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    FormsModule,
+    HttpClientModule,
+    CommonModule,
     BrowserModule
   ],
   providers: [],
